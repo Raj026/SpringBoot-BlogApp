2c25ce3d52d2ca7227034c2fb8276c024c4d343f
Raj026 <rajpatani31@gmail.com>
2024-08-27 17:14:51 +0530

    Implemented Paging and Sorting for the GetMapping method retrieving all the Posts

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -1,6 +1,7 @@
 package com.springboot.blog.controller;
 
 import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.payload.PostResponse;
 import com.springboot.blog.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,11 +25,12 @@ public class PostController {
     //Get All Posts of Blog Application --> Applying the Pagination in the get request.
     //localhost:8080/api/posts?pageNo=1&pageSize=5 this will show the records in chunks with 5 in quantity.
     @GetMapping()
-    public ResponseEntity<List<PostDto>> getAllPosts(
+    public PostResponse getAllPosts(
             @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
-            @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize
+            @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = "id", required = false) String sortBy
     )  {
-        return new ResponseEntity<>(postService.getAllPosts(pageNo, pageSize), HttpStatus.FOUND);
+        return postService.getAllPosts(pageNo, pageSize, sortBy);
     }
 
     //Get Post By Id
--- src/main/java/com/springboot/blog/service/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/service/PostServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -32,26 +33,26 @@ public class PostServiceImpl implements PostService{
     }
 
     @Override
-    public List<PostDto> getAllPosts(int pageNo, int pageSize) {
+    public PostResponse getAllPosts(int pageNo, int pageSize, String sortBy) {
 
         //create Pageable Instance
-        Pageable pageable = PageRequest.of(pageNo, pageSize);
-
+//        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
         Page<Post> posts = postRepository.findAll(pageable);
 
         //get content for page object
         List<Post> listOfPosts = posts.getContent();
 
-//        List<PostDto> content =  listOfPosts.stream().map(this::mapToDto).collect(Collectors.toList());
-//        PostResponse postResponse = new PostResponse();
-//        postResponse.setContent(content);
-//        postResponse.setPageNo(posts.getNumber());
-//        postResponse.setPageSize(posts.getSize());
-//        postResponse.setTotalElements(posts.getTotalElements());
-//        postResponse.setTotalPages(posts.getTotalPages());
-//        postResponse.setLast(posts.isLast());
-//
-//        return PostResponse;
+        List<PostDto> content =  listOfPosts.stream().map(this::mapToDto).collect(Collectors.toList());
+        PostResponse postResponse = new PostResponse();
+        postResponse.setContent(content);
+        postResponse.setPageNo(posts.getNumber());
+        postResponse.setPageSize(posts.getSize());
+        postResponse.setTotalElements(posts.getTotalElements());
+        postResponse.setTotalPages(posts.getTotalPages());
+        postResponse.setLast(posts.isLast());
+
+        return postResponse;
     }
 
     @Override
